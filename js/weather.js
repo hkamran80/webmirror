39791d24ffe1aaabc83ad9878acca47f3f2e12a6
H. Kamran <hkamran80@users.noreply.github.com>
2018-02-05 09:37:43 -0800

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,24 @@
-jQuery(document).ready(function($) {
-	$.ajax({
-	
-	var key = "";
-	var stateinitials = "";
-	var weather_city = "";
-		
-	url : "http://api.wunderground.com/api/" + key + "/geolookup/conditions/q/" + stateinitials + "/" + weather_city + ".json",
-	dataType : "jsonp",
-	success : function(parsed_json) {
-		
-	var city = parsed_json['location']['city'];
-	var state = parsed_json['location']['state'];
-	var temp_f = parsed_json['current_observation']['temp_f'];
-	var locX = city + ', ' + state;
-	var tempX = temp_f + '&deg;F';
+function weathery() {
+	jQuery(document).ready(function($) {
+		$.ajax({
 
-	document.getElementById("city").innerHTML = locX;
-	document.getElementById("temp").innerHTML = tempX;
-	}
+		var key = "";
+		var stateinitials = "";
+		var weather_city = "";
+
+		url : "http://api.wunderground.com/api/" + key + "/geolookup/conditions/q/" + stateinitials + "/" + weather_city + ".json",
+		dataType : "jsonp",
+		success : function(parsed_json) {
+
+		var city = parsed_json['location']['city'];
+		var state = parsed_json['location']['state'];
+		var temp_f = parsed_json['current_observation']['temp_f'];
+		var locX = city + ', ' + state;
+		var tempX = temp_f + '&deg;F';
+
+		document.getElementById("city").innerHTML = locX;
+		document.getElementById("temp").innerHTML = tempX;
+		}
+		});
 	});
-});
+}
